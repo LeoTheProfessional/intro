7a546cab1af9a51d3980ef9dd0f514bb5fe8d2b7
leo davis <leob7v@gmail.com>
2023-05-25 13:09:23 -0700

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace codin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("unga_cal");
+        }
+    }
+}
